27d9f7fd57346cff8daa14b509a848d91def03ca
AlbaAkhm <aalba.13@bk.ru>
2021-11-12 05:11:02 +0300

    homework number four

---
+++ src/main/java/lesson4/HomeWork4.java
@@ -0,0 +1,32 @@
+package lesson4;
+
+import java.util.Scanner;
+
+public class HomeWork4 {
+    public static void main(String[] args) {
+
+        //этот метод будет извлекать корень квадратный из введенного числа a
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число a = ");
+        int num = scanner.nextInt();
+        if (num < 0) {
+            System.out.println("Нельзя извлекать квадратный корень из отрицательного числа");
+            System.exit(0);
+        }
+
+        int squareRoot = 0;
+        int k = 1;
+        int i = num;
+        while (i > 0) {
+            i = i - k;
+            k = k + 2;
+            squareRoot = squareRoot + 1;
+        }
+        if (num != squareRoot * squareRoot) {
+            System.out.println("Вы ввели число, из которого квадратный корень не извлекается целым числом");
+        } else {
+            System.out.println("Корень квадратный из числа" + " " + num + " будет равен" + " " + squareRoot);
+        }
+    }
+}
